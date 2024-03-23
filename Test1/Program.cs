465dc466818b5e05e465ead018f94f77931bfe63
s22381 <s22381@pjwstk.edu.pl>
2024-03-23 23:30:38 +0100

    Modify variable

--- Test1/Program.cs
+++ Test1/Program.cs
@@ -42,12 +42,12 @@ static double CalculateAverage(int[] numbers)
         return 0; // Zwraca 0 dla pustej tablicy
     }
 
-    double sum = 0;
+    double total = 0;
     foreach (int number in numbers)
     {
-        sum += number;
+        total += number;
     }
-    return sum / numbers.Length;
+    return total / numbers.Length;
 }
 
 int[] numbers = { 1, 2, 3, 4, 5 };
