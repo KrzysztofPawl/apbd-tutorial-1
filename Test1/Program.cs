6774359ef7d8dea4686446a5a058e7ea3831da2a
s22381 <s22381@pjwstk.edu.pl>
2024-03-23 22:51:50 +0100

    Modyfikacja 2

--- Test1/Program.cs
+++ Test1/Program.cs
@@ -3,6 +3,10 @@ Console.WriteLine("Hello, World!");
 
 Console.WriteLine("Please enter your name: ");
 
-string name = Console.ReadLine();
+string? name = Console.ReadLine();
 
 Console.WriteLine($"Hello, {name}!");
+
+//Mod 2
+Console.WriteLine("Choose an option:\n1. Add two numbers\n2. Multiply two numbers");
+int choice = Convert.ToInt32(Console.ReadLine());
